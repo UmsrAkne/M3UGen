13635daa6fabf5e11ac3937fbd17740e320e0a85
Akn <sibata486@gmail.com>
2023-03-26 00:10:55 +0900

    init

---
+++ M3UGen/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,11 @@
+﻿using Prism.Mvvm;
+
+namespace M3UGen.ViewModels
+{
+    public class MainWindowViewModel : BindableBase
+    {
+        private string title = "Prism Application";
+
+        public string Title { get => title; set => SetProperty(ref title, value); }
+    }
+}
---
+++ M3UGen/Views/MainWindow.xaml.cs
@@ -0,0 +1,13 @@
+﻿namespace M3UGen.Views
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
