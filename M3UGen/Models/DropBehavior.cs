7f89fc67e11f2bd6d34c917acda38bb567aa2e03
Akn <sibata486@gmail.com>
2023-03-26 00:41:38 +0900

    add file / ドラッグアンドドロップの処理を追加

---
+++ M3UGen/Models/DropBehavior.cs
@@ -0,0 +1,70 @@
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Windows;
+using M3UGen.ViewModels;
+using Microsoft.Xaml.Behaviors;
+
+namespace M3UGen.Models
+{
+    public class DropBehavior : Behavior<Window>
+    {
+        protected override void OnAttached()
+        {
+            base.OnAttached();
+
+            // ファイルをドラッグしてきて、コントロール上に乗せた際の処理
+            AssociatedObject.PreviewDragOver += AssociatedObject_PreviewDragOver;
+
+            // ファイルをドロップした際の処理
+            AssociatedObject.Drop += AssociatedObject_Drop;
+        }
+
+        protected override void OnDetaching()
+        {
+            base.OnDetaching();
+            AssociatedObject.PreviewDragOver -= AssociatedObject_PreviewDragOver;
+            AssociatedObject.Drop -= AssociatedObject_Drop;
+        }
+
+        private void AssociatedObject_Drop(object sender, DragEventArgs e)
+        {
+            // ファイルパスの一覧の配列
+            string[] files = (string[])e.Data.GetData(DataFormats.FileDrop);
+            if (files == null)
+            {
+                return;
+            }
+
+            var fileInfos = new List<FileInfo>();
+
+            foreach (string p in files)
+            {
+                if (string.IsNullOrWhiteSpace(p))
+                {
+                    continue;
+                }
+
+                if (Directory.Exists(p))
+                {
+                    fileInfos.AddRange(Directory.GetFiles(p).Select(fp => new FileInfo(fp)));
+                }
+                else
+                {
+                    fileInfos.Add(new FileInfo(p));
+                }
+            }
+
+            if (sender is Window window)
+            {
+                ((MainWindowViewModel)window.DataContext).AddFiles(fileInfos);
+            }
+        }
+
+        private void AssociatedObject_PreviewDragOver(object sender, DragEventArgs e)
+        {
+            e.Effects = DragDropEffects.Copy;
+            e.Handled = e.Data.GetDataPresent(DataFormats.FileDrop);
+        }
+    }
+}
--- M3UGen/ViewModels/MainWindowViewModel.cs
+++ M3UGen/ViewModels/MainWindowViewModel.cs
@@ -1,11 +1,31 @@
-﻿using Prism.Mvvm;
+﻿using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.IO;
+using Prism.Mvvm;
 
 namespace M3UGen.ViewModels
 {
+    // ReSharper disable once ClassNeverInstantiated.Global
     public class MainWindowViewModel : BindableBase
     {
         private string title = "Prism Application";
 
+        private ObservableCollection<FileInfo> files = new ObservableCollection<FileInfo>();
+
         public string Title { get => title; set => SetProperty(ref title, value); }
+
+        public ObservableCollection<FileInfo> Files
+        {
+            get => files;
+            set => SetProperty(ref files, value);
+        }
+
+        public void AddFiles(IEnumerable<FileInfo> fileInfoList)
+        {
+            foreach (FileInfo f in fileInfoList)
+            {
+                Files.Add(f);
+            }
+        }
     }
 }
